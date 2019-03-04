bd59b3ad820937d73774feee552a6091a78675eb
Steve Hawken <steve@primaryvfx.com>
2019-03-04 11:28:56 +0000

    fixed script

--- ffmpeg/python/seqToMov.py
+++ ffmpeg/python/seqToMov.py
@@ -12,13 +12,13 @@ origFileName = os.path.basename( file )
 
 ##extract path to file
 path = os.path.dirname( file )
+drive = path.split('/')[0]
 
 ##rebuild path and file name, replacing frame number with %04d
 fname, fileExt = os.path.splitext(origFileName)
 fileName = '_'.join(fname.split('_')[:-1])
 filePadding = '_%4d'
-newFileName = path + '/' + fileName + filePadding + fileExt
-newFileName = newFileName.replace('/','\\')
+newFileName = fileName + filePadding + fileExt
 
 ##create dir for mp4
 movPath = path+'/'+'mp4/' 
@@ -27,11 +27,8 @@ if not os.path.exists( movPath ):
 
 ## create mp4 name and path
 movFileName = '_'.join(origFileName.split('_')[:-1]) + '.mp4'
-movFile = movPath + movFileName
-#movFile = movFile.replace('/','\\')
-
+movFile = 'mp4/' + movFileName
 
 ##build ffmpeg cmd 
-cmd="ffmpeg -i %s -framerate 25 -s 1920x1080 -vcodec libx264 -r 25 -b:v 4M -pix_fmt yuv420p %s" %(newFileName,movFile)
-ffmpeg -i spectrum_sh01_comp_v04_%4d.dpx -framerate 25 -s 1920x1080 -vcodec libx264 -r 25 -b:v 4M -pix_fmt yuv420p test1.mp4
+cmd="%s&& cd %s&& ffmpeg -i %s -s 1920x1080 -vcodec libx264 -crf 25 -b:v 4M -pix_fmt yuv420p %s" %(drive,path,newFileName,movFile)
 sp.call(cmd,shell=True)
