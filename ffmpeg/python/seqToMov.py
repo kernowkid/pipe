3e88189b73ea958fa199f2625d9d5c83ba5da653
Steve Hawken <steve@primaryvfx.com>
2019-03-04 12:23:19 +0000

    back tracked to first plan with an updated ffmpeg command

--- ffmpeg/python/seqToMov.py
+++ ffmpeg/python/seqToMov.py
@@ -10,25 +10,32 @@ file = file.replace('\\','/')
 ##file name
 origFileName = os.path.basename( file )
 
+
 ##extract path to file
 path = os.path.dirname( file )
-drive = path.split('/')[0]
+
 
 ##rebuild path and file name, replacing frame number with %04d
 fname, fileExt = os.path.splitext(origFileName)
 fileName = '_'.join(fname.split('_')[:-1])
-filePadding = '_%4d'
-newFileName = fileName + filePadding + fileExt
+startNumber = fname.split('_')[-1]
+filePadding = '_%04d'
+newFileName = path + '/' + fileName + filePadding + fileExt
+newFileName = newFileName.replace('/','\\')
+
 
 ##create dir for mp4
 movPath = path+'/'+'mp4/' 
-if not os.path.exists( movPath ):
-        os.mkdir( movPath )
+#if not os.path.exists( movPath ):
+#        os.mkdir( movPath )
+
 
 ## create mp4 name and path
 movFileName = '_'.join(origFileName.split('_')[:-1]) + '.mp4'
-movFile = 'mp4/' + movFileName
+movFile = movPath + movFileName
+movFile = movFile.replace('/','\\')
+
 
 ##build ffmpeg cmd 
-cmd="%s&& cd %s&& ffmpeg -i %s -s 1920x1080 -vcodec libx264 -crf 25 -b:v 4M -pix_fmt yuv420p %s" %(drive,path,newFileName,movFile)
+cmd="ffmpeg -framerate 25 -start_number %s -i %s -s 1920x1080 -c:v libx264 -crf 25 -b:v 4M -pix_fmt yuv420p %s" %(startNumber,newFileName,movFile)
 sp.call(cmd,shell=True)
